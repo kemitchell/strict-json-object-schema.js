051ac1f6f24b667fa5497a2a93ab7538c610b001
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-20 21:11:00 -0700

    Set missing type to object

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function (properties) {
-  var schema = {properties: properties}
+  var schema = {type: 'object', properties: properties}
   schema.required = Object.keys(properties)
   schema.additionalProperties = false
   return schema
